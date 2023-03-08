06f5c8cf317693da64db6cd76e7ed643b50b0e76
Al3ksanderD <olo1045@wp.pl>
2023-03-08 17:03:20 +0100

    ...

--- src/main/java/com/codecool/supersplinter3000/controller/UserStoryController.java
+++ src/main/java/com/codecool/supersplinter3000/controller/UserStoryController.java
@@ -6,12 +6,14 @@ import com.codecool.supersplinter3000.service.UserStoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -49,7 +51,10 @@ public class UserStoryController {
     }
 
     @PostMapping("/new-user-story")
-    public String saveOrUpdateNewUserStory(UserStory userStory){
+    public String saveOrUpdateNewUserStory(@Valid UserStory userStory, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "new_user_story";
+        }
         userStoryService.addOrUpdateNewUserStory(userStory);
         return "redirect:/";
     }
--- src/main/java/com/codecool/supersplinter3000/entity/UserStory.java
+++ src/main/java/com/codecool/supersplinter3000/entity/UserStory.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @Data
 @NoArgsConstructor
@@ -16,9 +17,14 @@ public class UserStory {
             strategy = GenerationType.IDENTITY
     )
     private Long id;
+    @Size(min = 5, max = 50, message = "Title must be between 5 and 50!")
     private String title;
+    @NotBlank(message = "Cannot be empty!")
     private String story;
     private String acceptanceCriteria;
+    @DecimalMin(value = "0.5", message = "Min 0.5")
+    @DecimalMax(value = "40.0", message = "Max 40")
+    @NotEmpty(message = "Cannot be empty!")
     private Double estimation;
     private Integer businessValue = 100;
     @Enumerated(EnumType.STRING)
